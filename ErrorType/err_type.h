4a2a0b60b029dba6962a5943a27c1d7b667f97f9
yuanchenghao1108 <648588527@qq.com>
2022-04-05 12:23:17 +0800

    Create err_type.h

---
+++ ErrorType/err_type.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef ERR_TYPE_H
+#define ERR_TYPE_H
+// -------------------------------------------------------------------------------
+#define ERR_SUCCESS															(0)
+#define ERR_FAILED															(-1)
+#define ERR_FILE_NAME_NULL													(-2)
+#define ERR_FILE_OPEN_FAIL													(-3)
+#define ERR_OPEN_PORT_FAIL													(-4)
+#define ERR_CREATE_MEMORY_SIZE												(-5)
+#define ERR_INPUT_PARAMETER												    (-6)
+#define ERR_NULL_HANDLE												        (-7)
+#define ERR_SEND_FAIL												        (-8)
+#define ERR_READ_FAIL												        (-9)
+#define ERR_MERRORY_SIZE												    (-10)
+
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------
+#endif // !ERR_TYPE_H
